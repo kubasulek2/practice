d4bae22a7308da8c712de0bb4c7513e29690f092
Kuba Sukowski <kubasulek2@gmail.com>
2019-05-29 14:30:23 +0200

    some exercises

--- public/js/main.js
+++ public/js/main.js
@@ -1,2 +1,2 @@
-$(function(){$(".anim").on("click",function(){let a=$(this).attr("data-anim");console.log(a),$("#cube").removeClass().addClass(a)})});
+
 //# sourceMappingURL=main.js.map
--- src/js/main.js
+++
@@ -1,7 +0,0 @@
-$(function(){
-  $('.anim').on('click',function(){
-    let anim = $(this).attr('data-anim');
-    console.log(anim);
-    $('#cube').removeClass().addClass(anim);
-  });
-});
