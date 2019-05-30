c88157658d9fcdf205beb3482ab6afd34fa6a968
Kuba Sukowski <kubasulek2@gmail.com>
2019-05-30 19:04:25 +0200

    animation project in progress

--- src/js/main.js
+++ src/js/main.js
@@ -1,67 +1,125 @@
-$(() => {
+class htmlElement {
+  constructor(element){
+    this.element = element
+  }
+  copy(){
+    return this.element.clone()
+  }
+  remove(){
+    this.element.remove()
+  }
+  append(parent){
+      this.element.append(parent)
+  }
+}
 
-  let planes = $('#top-layer>div');
+class tetragon3d extends htmlElement{
+  constructor(element ,speed){
+    super(element);
+    this.rotationSpeed = speed;
 
-  planes.on('click', function () {
-    const plane = $(this).attr("id");
-    const parentElement = $(this).parent();
-    let rotation;
+    this.planes = this.element.children('div');
+    this.timeData = {
+      then: undefined,
+      now: undefined
+    };
+    this.motionData = {
+      currentAngle: 0,
+      targetAngle: undefined,
+      speed: undefined,
+      move: true
 
-    switch (plane) {
+    }
+  }
+
+  animateRotationY() {
+
+    if ( this.motionData.move ){
+      this.motionData.speed = this.computeRotatingTime( 45 );
+      this.motionData.currentAngle = this.motionData.currentAngle < -360 ? 0 : this.motionData.currentAngle;
+      this.motionData.currentAngle -= this.rotationSpeed;
+
+      this.element.css( "transform", ` translateZ(-250px) rotateY(${this.motionData.currentAngle}deg)` );
+
+      window.requestAnimationFrame( ()=> this.animateRotationY() )
+    }
+  }
+
+  computeTransitionSpeed(){
+    let distance = Math.abs(this.motionData.targetAngle) - Math.abs(this.motionData.currentAngle);
+    let ratio = distance / 45;
+    return ratio * this.motionData.speed
+  }
+
+  computeRotatingTime(angle){
+
+    let rotationTime;
+    let measure = Math.abs( Math.floor(this.motionData.currentAngle) );
+
+    if (Number(measure) === 1) this.timeDatathen = new Date();
+    else if (measure === 1 + angle)  this.timeDatanow = new Date;
+    if(this.timeDatanow && this.timeDatathen) {
+      rotationTime =  (this.timeDatanow - this.timeDatathen)/1000;
+      this.timeDatanow = undefined;
+      this.timeDatathen = undefined;
+      return rotationTime;
+    }
+    return this.motionData.speed;
+  }
+
+  getTargetAngle (e) {
+
+    const targetId = $(e.target).attr("id");
+
+    switch (targetId) {
       case "front":
-        rotation = 0;
+        this.motionData.targetAngle = 0;
         break;
       case "right":
-        rotation = -90;
+        this.motionData.targetAngle = -90;
         break;
       case "back":
-        rotation = -180;
+        this.motionData.targetAngle = -180;
         break;
       case "left":
-        rotation = -270;
+        this.motionData.targetAngle = -270;
         break;
     }
-    parentElement
-      .css("animation-play-state", 'paused')
-      //.css('transform', `translateZ(-250px) rotateY(${rotation}deg)`)
-    $(this).addClass("focus");
-
-  });
-
-  function animateRotationY(obj, startAngle, timeInSec) {
-    let frame = (timeInSec *1000 / 360).toFixed(4);
-    let deg = startAngle;
-
-
-
-      const animation = window.setInterval(()=>{
-        deg = deg < -360 ? 0 : deg;
-        deg++;
-        console.log(deg);
-        obj.css("transform", ` translateZ(-250px) rotateY(${deg}deg)`)
-      },frame)
-
   }
-  //animateRotationY($('#top-layer'), 0, 6);
 
+  planeClickEvent(flag){
+    if(flag){
+      this.planes.on('click', (e)=> {
+        this.getTargetAngle(e);
 
-  function getRotationDegrees(obj) {
-    let angle;
-    let matrix = obj.getComputedStyle("-webkit-transform") ||
-      obj.getComputedStyle("-moz-transform")    ||
-      obj.getComputedStyle("-ms-transform")     ||
-      obj.getComputedStyle("-o-transform")      ||
-      obj.getComputedStyle("transform");
-    if(matrix !== 'none') {
-      let values = matrix.split('(')[1].split(')')[0].split(',');
-      let a = values[0];
-      let b = values[1];
-      angle = Math.round(Math.atan2(b, a) * (180/Math.PI));
+        let transitionSpeed = this.computeTransitionSpeed();
 
+        this.motionData.move = false;
+
+        this.element
+          .css('transition', `all ${transitionSpeed}s ease-out`)
+          .css('transform', `translateZ(-250px) rotateY(${ this.motionData.targetAngle }deg)`)
+
+      })
     } else {
-      angle = 0;
+      this.planes.off()
     }
-    return (angle < 0) ? angle + 360 : angle;
+
   }
 
+
+
+}
+
+$(() => {
+  let myElement = new tetragon3d($('#top-layer'),0.8);
+  myElement.animateRotationY();
+  myElement.planeClickEvent(true);
+
+  Math.easeOutQuad = function (t, b, c, d) {
+    t /= d;
+    return -c * t*(t-2) + b;
+  };
+
+  let easing = Math.easeOutQuad(0.6,54,36,0.4);
 });
