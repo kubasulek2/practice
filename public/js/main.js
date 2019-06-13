33aa5722bb97d616d86d927bc163f1212c598251
Kuba Sukowski <kubasulek2@gmail.com>
2019-06-13 21:00:34 +0200

    staggerFromTo, labels label-delay, delay function, event callback with parameters

--- public/js/main.js
+++ public/js/main.js
@@ -1,2 +1,2 @@
-$(()=>{TweenMax.set(".oval",{x:100}),new TimelineMax({repeat:-1,yoyo:!0}).to($(".oval"),1,{x:-100})});
+$(()=>{TweenMax.set(".oval",{x:100}),new TimelineMax({repeat:-1,yoyo:!0}).to($(".oval"),1,{x:-100}),TweenMax.staggerFromTo(".triangle",1,{rotation:90,y:200,opacity:0},{rotation:0,y:0,opacity:1,ease:Back.easeOut},.2),(new TimelineMax).to(".circle",1,{x:100,ease:Bounce.easeOut},"first").to(".square",1,{x:100,ease:Bounce.easeOut,delay:.5}).to(".rectangle",1,{x:100,ease:Bounce.easeOut},"first+=0.25").delay(2).eventCallback("onComplete",function(){console.log(arguments[0])},["info"])});
 //# sourceMappingURL=main.js.map
--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,30 @@
 $(() => {
 	TweenMax.set('.oval', { x: 100 });
 
-
 	const tlAnimation = new TimelineMax({ repeat: -1, yoyo: true });
-	tlAnimation.to($('.oval'), 1, { x: -100 })
+	tlAnimation.to($('.oval'), 1, { x: -100 });
+
+	TweenMax.staggerFromTo('.triangle', 1, {
+		rotation: 90,
+		y: 200,
+		opacity: 0,
+	}, {
+			rotation: 0,
+			y: 0,
+			opacity: 1,
+			ease: Back.easeOut
+		}, 0.2)
+
+	const tlShapes = new TimelineMax();
+	tlShapes
+		.to('.circle', 1, { x: 100, ease: Bounce.easeOut },'first')
+		.to('.square', 1, { x: 100, ease: Bounce.easeOut, delay: .5 })
+		.to('.rectangle', 1, { x: 100, ease: Bounce.easeOut },'first+=0.25')
+		.delay(2)
+		.eventCallback('onComplete', function(){
+			console.log(arguments[0]);		
+		},['info'])
+
+
+		
 });
