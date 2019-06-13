74a14f447f29a052bd1cc3379cea03f1c0cbefba
Kuba Sukowski <kubasulek2@gmail.com>
2019-06-13 17:25:50 +0200

    gsap some work

--- public/js/main.js
+++ public/js/main.js
@@ -1,2 +1,2 @@
-$(()=>{TweenMax.to(".circle",3,{x:150,y:-100,backgroundColor:"red"}),TweenMax.to(".box",2,{x:-150,y:-100,scale:2,delay:1,ease:Back.easeOut})});
+$(()=>{TweenMax.to($(".circle"),3,{x:"150%",backgroundColor:"red"})});
 //# sourceMappingURL=main.js.map
--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,16 @@
 
 $(() => {
-  TweenMax.to('.circle', 3, { 
-    x: 150,
-    y: -100,
+  let a = 5;
+  TweenMax.to($('.circle'), 3, { 
+    x: '150%',
     backgroundColor: 'red' 
   });
-  TweenMax.to('.box', 2, { 
-    x: -150,
-    y: -100,
-    scale: 2 ,
-    delay: 1,
-    ease: Back.easeOut
-  });
+  // TweenMax.to('.box', 2, { 
+  //   x: -150,
+  //   y: -100,
+  //   scale: 2 ,
+  //   delay: 1,
+  //   ease: Back.easeOut
+  // });
 
 });
