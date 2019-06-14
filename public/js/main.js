2d223be26245469e3c1133d035b5f59b61b93495
Kuba Sukowski <kubasulek2@gmail.com>
2019-06-14 15:07:30 +0200

    alpha star menu in progress

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,32 @@
 $(() => {
-	let b = 2;
-	let a = `str${b}`;
-	let c = 0;
-	a.toUpperCase().replace('STR', c);
+	const burgerIcon = $('.burgerIcon');
+	const tml = new TimelineMax({ paused: true });
 
-	console.log();
+	burgerIcon.hover(function () {
+		TweenMax.to(burgerIcon.children()[0], .2, { x: 10 });
+		TweenMax.to(burgerIcon.children()[2], .2, { x: -10 });
+	}, function () {
+		TweenMax.to(burgerIcon.children()[0], .2, { x: 0 });
+		TweenMax.to(burgerIcon.children()[2], .2, { x: 0 });
+	});
+	tml.to('nav', .5, {
+		autoAlpha: 1
+	}).staggerFromTo('nav li', .6, {
+		opacity: 0,
+		y: 100
+
+	}, {
+		opacity: 1,
+		y: 0,
+		ease: Back.easeOut
+	}, .1);
+
+	$(burgerIcon).on('click', function () {
+		tml.play(0);
+	});
+
+	$('.closeButton').on('click', function () {
+		tml.reverse(0);
+	});
 });
 //# sourceMappingURL=main.js.map
