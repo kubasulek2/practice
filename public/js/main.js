3dbf38c0db223365e61cd32b11eb38e76ab6ba63
Kuba Sukowski <kubasulek2@gmail.com>
2019-08-13 01:23:48 +0200

    algorithms a

--- public/js/main.js
+++ public/js/main.js
@@ -269,4 +269,185 @@ $(() => {
 	//.addIndicators() // add indicators (requires plugin)
 	.addTo(controller);
 });
+
+const arrReverse = arr => {
+
+	for (let i = 0; i < arr.length / 2; i++) {
+		const prev = i,
+		      next = arr.length - 1 - i,
+		      temp = arr[i];
+
+		arr[prev] = arr[next];
+		arr[next] = temp;
+	}
+	return arr;
+};
+let array = [1, 2, 3, 4, 5, 6];
+
+let reversedArray = arrReverse(array);
+console.log(reversedArray);
+
+const arrToSort = [4, -100, 25, -6, -80, 22, 44, 7, 1000, 44];
+console.log('arr ' + arrToSort);
+
+//49 operations
+const sortArr = arr => {
+	let array = [...arr],
+	    returnArr = [],
+	    n = array.length;
+
+	for (let i = 0; i < n; i++) {
+		let minValue = array[0],
+		    minIndex = 0;
+		for (let j = 1; j < array.length; j++) {
+			console.log('sort n');
+
+			if (array[j] < minValue) {
+				minValue = array[j];
+				minIndex = j;
+			}
+		}
+		returnArr.push(minValue);
+		array.splice(minIndex, 1);
+	}
+	return returnArr;
+};
+
+//49 operations
+const bubbleSort = arr => {
+	let array = [...arr];
+	let temp;
+	for (let i = 0; i < array.length - 1; i++) {
+
+		for (let j = 0; j < array.length - 1 - i; j++) {
+
+			if (array[j] > array[j + 1]) {
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+			}
+			console.log('n bubble');
+		}
+	}
+	return array;
+};
+
+//19 operations
+const insertionSort = arr => {
+	let array = [...arr];
+	for (let i = 1; i < array.length; i++) {
+		const temp = array[i];
+		let counter = 0;
+		inner: for (let j = i - 1; j >= 0; j--) {
+			console.log('insertion n');
+			if (temp >= array[j]) break inner;else {
+				array[i - counter] = array[j];
+				array[j] = temp;
+				counter++;
+			}
+		}
+	}
+	return array;
+};
+
+let sortedArray = bubbleSort(arrToSort);
+console.log(sortedArray);
+sortedArray = sortArr(arrToSort);
+console.log(sortedArray);
+sortedArray = insertionSort(arrToSort);
+console.log(sortedArray);
+
+// Create Array from 1 to n
+const binaryArr = [...Array(100).keys()].map(val => ++val); // ++val to increment before return, val++ not working
+
+
+//binary search!!
+const biSearch = (arr, val, minIndex, maxIndex) => {
+	console.log('n');
+
+	minIndex = minIndex || 0;
+	maxIndex = maxIndex || arr.length;
+
+	const mid = minIndex + Math.floor((maxIndex - minIndex) / 2);
+	//console.log('n');
+
+	if (val === arr[mid]) {
+
+		return mid;
+	} else if (val > arr[mid] && mid < arr.length - 1) {
+
+		return biSearch(arr, val, mid, maxIndex);
+	} else if (val < arr[mid] && mid > 0) {
+		return biSearch(arr, val, minIndex, mid);
+	} else {
+		return 'value not in the array';
+	}
+};
+
+let findIndex = biSearch(binaryArr, 100);
+console.log(findIndex);
+
+let arr2 = [4, 1, -18, 24, 3, 4, 1, -3, 2];
+
+const sortArrWholeNoRepeat = arr => {
+	let array = [...arr],
+	    returnArr = [],
+	    n = array.length;
+
+	for (let i = 0; i < n; i++) {
+		let minValue = array[0],
+		    minIndex = 0;
+		for (let j = 1; j < array.length; j++) {
+			console.log('sort n');
+
+			if (array[j] < minValue) {
+				minValue = array[j];
+				minIndex = j;
+			}
+		}
+		repeatCheck: if (minValue >= 0) {
+			for (let k = 0; k < returnArr.length; k++) {
+				if (minValue === returnArr[k]) break repeatCheck;
+			}
+			returnArr.push(minValue);
+		}
+		array.splice(minIndex, 1);
+	}
+	for (let i = 0; i < returnArr.length; i++) {
+
+		if (returnArr[i] !== i) return i;
+	}
+};
+
+let wholeArr = sortArrWholeNoRepeat(arr2);
+console.log(wholeArr);
+
+const matchingPar = (arr, sum) => {
+	for (let i = 0; i < arr.length; i++) {
+		for (let j = i + 1; j < arr.length; j++) {
+			if (arr[i] + arr[j] === sum) return [arr[i], arr[j]];
+		}
+	}
+	return false;
+};
+const arr3 = [1, 2, 3, 4, 5];
+
+console.log(matchingPar(arr3, 5));
+
+const binaryArray = [1, 0, 1, 0, 1, 0, 1, 1, 0, 0];
+
+const binarySort = arr => {
+	let counter = -1;
+	let returnArr = [];
+	for (let i = 0; i < arr.length; i++) {
+		if (arr[i] === 0) counter++;
+	}
+	for (let j = 0; j < arr.length; j++) {
+		if (j <= counter) returnArr.push(0);else returnArr.push(1);
+	}
+	return returnArr;
+};
+
+const binarySorted = binarySort(binaryArray);
+console.log(binarySorted);
 //# sourceMappingURL=main.js.map
--- src/js/main.js
+++ src/js/main.js
@@ -156,10 +156,10 @@
 // 			endIndex = n -1 - i,
 // 			temp = arr[startIndex];
 
-		
+
 // 		arr[startIndex] = array[endIndex];
 // 		arr[endIndex] = temp; 
-		
+
 
 
 // 	}
@@ -171,25 +171,25 @@
 // let arrayToSort = [10,7,2,5,8,4,2];
 
 // function sortArray(arr) {
-		
+
 // 	let sorted = [],
 // 		n = arr.length;
-		
+
 // 	for (let i = 0; i < n; i++) {
 // 		let min = arr[0],
 // 			minIndex = 0;
 // 		for (let j = 1; j < arr.length; j++) {
-			
+
 // 			if (arr[j] < min){
 // 				min = arr[j];
 // 				minIndex = j;
 // 			}
-			
+
 // 		}
-		
+
 // 		sorted.push(min);
 // 		arr.splice(minIndex,1);
-		
+
 // 	}
 // 	return sorted;
 // }
@@ -203,7 +203,7 @@ $(() => {
 		triggerHook: 1,
 	})
 		.duration('200%')
-		.setTween( TweenMax.from('#parallax', 1, {
+		.setTween(TweenMax.from('#parallax', 1, {
 			y: '-50%',
 			ease: Linear.easeNone
 		}))
@@ -216,7 +216,7 @@ $(() => {
 	})
 		.setPin('#slidein')
 		.addIndicators() // add indicators (requires plugin)
-		.addTo(controller);	
+		.addTo(controller);
 
 	new ScrollMagic.Scene({
 		triggerElement: '#slidein2',
@@ -224,19 +224,19 @@ $(() => {
 	})
 		.setPin('#slidein2')
 		.addIndicators() // add indicators (requires plugin)
-		.addTo(controller);	
-	
+		.addTo(controller);
+
 	const fromLeftTimeline = new TimelineMax();
 	const fromLeftFrom = TweenMax.from('#left', 1, {
 		x: -500
 	});
 	const fromLeftTo = TweenMax.to('#left', 1, {
 		x: 0
-	});	
+	});
 	fromLeftTimeline
 		.add(fromLeftFrom)
 		.add(fromLeftTo);
-	
+
 	new ScrollMagic.Scene({
 		triggerElement: '#slidein2',
 		offset: 200
@@ -258,17 +258,17 @@ $(() => {
 	fadeInTimeline
 		.add(fadeInFrom)
 		.add(fadeInTo);
- 
+
 	new ScrollMagic.Scene({
 		triggerElement: '#slidein2',
 		offset: 200,
 	})
 		.setTween(fadeInTimeline)
 		.duration(400)
-	//    .reverse(false)
-	//.addIndicators() // add indicators (requires plugin)
+		//    .reverse(false)
+		//.addIndicators() // add indicators (requires plugin)
 		.addTo(controller);
- 
+
 	//Fly in from the left
 	const fromBottomTimeline = new TimelineMax();
 	const fromBottomFrom = TweenMax.from('#bottom', 1, {
@@ -280,15 +280,218 @@ $(() => {
 	fromBottomTimeline
 		.add(fromBottomFrom)
 		.add(fromBottomTo);
- 
+
 	new ScrollMagic.Scene({
 		triggerElement: '#slidein2',
 		offset: 200,
 	})
 		.setTween(fromBottomTimeline)
 		.duration(400)
-	//    .reverse(false)
-	//.addIndicators() // add indicators (requires plugin)
-		.addTo(controller);	
+		//    .reverse(false)
+		//.addIndicators() // add indicators (requires plugin)
+		.addTo(controller);
 });
 
+const arrReverse = arr => {
+
+	for (let i = 0; i < arr.length / 2; i++) {
+		const
+			prev = i,
+			next = arr.length - 1 - i,
+			temp = arr[i];
+
+		arr[prev] = arr[next];
+		arr[next] = temp;
+
+	}
+	return arr;
+};
+let array = [1, 2, 3, 4, 5, 6];
+
+let reversedArray = arrReverse(array);
+console.log(reversedArray);
+
+const arrToSort = [4, -100, 25, -6, -80, 22, 44, 7, 1000, 44];
+console.log('arr ' + arrToSort);
+
+//49 operations
+const sortArr = arr => {
+	let array = [...arr],
+		returnArr = [],
+		n = array.length;
+
+	for (let i = 0; i < n; i++) {
+		let minValue = array[0],
+			minIndex = 0;
+		for (let j = 1; j < array.length; j++) {
+			console.log('sort n');
+
+			if (array[j] < minValue) {
+				minValue = array[j];
+				minIndex = j;
+			}
+		}
+		returnArr.push(minValue);
+		array.splice(minIndex, 1);
+	}
+	return returnArr;
+};
+
+//49 operations
+const bubbleSort = arr => {
+	let array = [...arr];
+	let temp;
+	for (let i = 0; i < array.length - 1; i++) {
+
+		for (let j = 0; j < array.length - 1 - i; j++) {
+
+			if (array[j] > array[j + 1]) {
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+			}
+			console.log('n bubble');
+
+		}
+	}
+	return array;
+};
+
+//19 operations
+const insertionSort = arr => {
+	let array = [...arr];
+	for (let i = 1; i < array.length; i++) {
+		const temp = array[i];
+		let counter = 0;
+		inner:
+		for (let j = i - 1; j >= 0; j--) {
+			console.log('insertion n');
+			if (temp >= array[j]) break inner;
+			else {
+				array[i - counter] = array[j];
+				array[j] = temp;
+				counter++;
+			}
+		}
+	}
+	return array;
+
+};
+
+let sortedArray = bubbleSort(arrToSort);
+console.log(sortedArray);
+sortedArray = sortArr(arrToSort);
+console.log(sortedArray);
+sortedArray = insertionSort(arrToSort);
+console.log(sortedArray);
+
+// Create Array from 1 to n
+const binaryArr = [...Array(100).keys()].map(val => ++val); // ++val to increment before return, val++ not working
+
+
+//binary search!!
+const biSearch = (arr, val, minIndex, maxIndex) => {
+	console.log('n');
+	
+	minIndex = minIndex || 0;
+	maxIndex = maxIndex || arr.length;
+
+	const mid = minIndex + Math.floor((maxIndex - minIndex) / 2);
+	//console.log('n');
+
+	if (val === arr[mid]) {
+
+		return mid;
+
+	} else if (val > arr[mid] && mid < arr.length - 1) {
+		
+		
+		return biSearch(arr, val, mid, maxIndex);
+
+	} else if (val < arr[mid] && mid > 0) {
+		return biSearch(arr, val, minIndex, mid);
+
+	} else {
+		return 'value not in the array';
+	}
+};
+
+let findIndex = biSearch(binaryArr, 100);
+console.log(findIndex);
+
+
+let arr2 = [4,1,-18,24,3,4,1,-3,2];
+
+const sortArrWholeNoRepeat = arr => {
+	let array = [...arr],
+		returnArr = [],
+		n = array.length;
+
+	for (let i = 0; i < n; i++) {
+		let minValue = array[0],
+			minIndex = 0;
+		for (let j = 1; j < array.length; j++) {
+			console.log('sort n');
+
+			if (array[j] < minValue) {
+				minValue = array[j];
+				minIndex = j;
+			}
+		}
+		repeatCheck:
+		if (minValue >= 0){ 
+			for (let k = 0; k < returnArr.length; k++) {
+				if(minValue === returnArr[k]) break repeatCheck;
+				
+			}
+			returnArr.push(minValue);
+		}
+		array.splice(minIndex, 1);
+	}
+	for (let i = 0; i < returnArr.length; i++) {
+	
+		if (returnArr[i] !== i) return i;
+		
+	}
+};
+
+let wholeArr = sortArrWholeNoRepeat(arr2);
+console.log(wholeArr);
+
+
+const matchingPar = (arr, sum) => {
+	for (let i = 0; i < arr.length; i++) {
+		for (let j = i+1; j < arr.length; j++) {
+			if (arr[i]+arr[j] === sum) return [arr[i], arr[j]];
+			
+		}
+	}
+	return false;
+};
+const arr3 = [1,2,3,4,5];
+
+console.log(matchingPar(arr3,5));
+
+
+
+const binaryArray = [1,0,1,0,1,0,1,1,0,0];
+
+
+const binarySort = arr => {
+	let counter = -1;
+	let returnArr= [];
+	for (let i = 0; i < arr.length; i++) {
+		if( arr[i] === 0) counter++;
+		
+	}
+	for (let j = 0; j < arr.length; j++) {
+		if (j <= counter) 
+			returnArr.push(0);
+		else
+			returnArr.push(1);	
+	}
+	return returnArr;
+};
+
+const binarySorted = binarySort(binaryArray);
+console.log(binarySorted);
